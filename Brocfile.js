af2915e463d4a80787a66d44954cc0058a1b5894
Hector Zarco <hezarco@gmail.com>
2014-09-13 22:40:05 +0200

    Remove bootstrap map

--- Brocfile.js
+++ Brocfile.js
@@ -5,6 +5,6 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 var app = new EmberApp();
 
 app.import('vendor/bootstrap/dist/css/bootstrap.css');
-app.import('vendor/bootstrap/dist/js/bootstrap.js');
+// app.import('vendor/bootstrap/dist/js/bootstrap.js');
 
 module.exports = app.toTree();

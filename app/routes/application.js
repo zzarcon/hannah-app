57d7c638d7fea652c444b20adf78a9922894e344
Hector Zarco <hezarco@gmail.com>
2014-10-19 00:37:41 +0200

    Fix application redirection

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,11 +1,5 @@
 import Ember from "ember";
 
 export default Ember.Route.extend({
-  redirect: function() {
-    if (this.get('session.isLogged')) {
-      this.transitionTo('dashboard');
-    } else {
-      this.transitionTo('login');
-    }
-  }
+
 });

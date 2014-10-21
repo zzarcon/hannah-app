9841fd24823ceabeb6891bac83bab84fe19ea4d0
Hector Zarco <hezarco@gmail.com>
2014-10-21 15:07:13 +0200

    Rename stuff

--- app/routes/campaigns.js
+++ app/routes/campaigns.js
@@ -2,7 +2,9 @@ import Ember from "ember";
 import AuthenticatedRoute from "../mixins/authenticated-route";
 
 export default Ember.Route.extend(AuthenticatedRoute, {
-  needsAuthentication: true
+  needsAuthentication: true,
 
-
+  model: function() {
+    return this.get('store').find('campaign');
+  }
 });

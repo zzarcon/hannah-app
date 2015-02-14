1aad308d5b6711eb4db627f0c7716de8ecb2c199
Hector Zarco <hezarco@gmail.com>
2015-02-14 20:02:38 +0100

    Add logout button

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,5 +1,10 @@
 import Ember from "ember";
 
 export default Ember.Route.extend({
-
+  actions: {
+    logout: function() {
+      localStorage.removeItem("session_token");
+      window.location.reload();
+    }
+  }
 });

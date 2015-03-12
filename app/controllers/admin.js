6290cd6871a1476047cc85f9dc060f628c87d90e
Hector Zarco <hezarco@gmail.com>
2015-03-12 22:12:37 +0100

    Sort users and use table for the UI

--- app/controllers/admin.js
+++ app/controllers/admin.js
@@ -1,10 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.ObjectController.extend({
+  usersSort: ['activeCampaigns:desc', 'username'],
   users: function() {
     return this.get('store').all('user');
   }.property(),
 
   //Only show registered users in the section
-  activeUsers: Ember.computed.filterBy('users', 'impersonable', true)
+  unsortedActiveUsers: Ember.computed.filterBy('users', 'impersonable', true),
+  activeUsers: Ember.computed.sort('unsortedActiveUsers', 'usersSort')
 });

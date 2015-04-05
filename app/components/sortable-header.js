288bbb0629e1f69dd94a800a965438d2dad23bc7
Hector Zarco <hezarco@gmail.com>
2015-04-05 21:24:59 +0200

    Enable sorting in admin columns

--- app/components/sortable-header.js
+++ app/components/sortable-header.js
@@ -2,17 +2,16 @@ import Ember from "ember";
 
 export default Ember.Component.extend({
   action: 'sort',
-
   sortDirectionBinding: 'targetObject.sortAscending',
   sortPropertyBinding: 'targetObject.sortProperty',
-
-  active: function(){
-    return this.get('sortProperty') == this.get('sortBy');
-  }.property('sortProperty'),
-
   tagName: 'th',
   sortBy: null,
   ascending: true,
+
+  active: function() {
+    return this.get('sortProperty') == this.get('sortBy');
+  }.property('sortProperty'),
+
   click: function(){
     this.sendAction('action', this.get('sortBy'),this.get('ascending') !== "false");
   }
--- app/controllers/admin.js
+++ app/controllers/admin.js
@@ -2,19 +2,19 @@ import Ember from 'ember';
 import SortableController from "../mixins/sortable-controller";
 
 export default Ember.ArrayController.extend(SortableController, {
-  sortProperties: ['activeCampaigns:desc', 'username'],
-  
+  sortProperty: 'subscriptionExpiresAt',
+  sortAscending: false,
+
   //Only show registered users in the section
-  // impersonableUsers: Ember.computed.filterBy('users', 'impersonable', true),
+  impersonableUsers: Ember.computed.filterBy('@this', 'impersonable', true),
   activeUsers: Ember.computed.filterBy('impersonableUsers', 'isExpired', false),
-  // sortedUsers: Ember.computed.sort('impersonableUsers', 'usersSort'),
 
   model: function() {
     return this.get('store').all('user');
   }.property(),
 
-  _totalCampaigns: function() {
-    return this.get('impersonableUsers').mapBy('activeCampaigns').reduce(function(prev, current) {
+  activeCampaigns: function() {
+    return this.get('activeUsers').mapBy('activeCampaigns').reduce(function(prev, current) {
       return prev + current;
     });
   }.property('impersonableUsers.length'),
@@ -26,9 +26,9 @@ export default Ember.ArrayController.extend(SortableController, {
       }
 
       user.save().then(function() {
-        swal("Success", "User saved", "success");
+        window.swal("Success", "User saved", "success");
       }).catch(function() {
-        sweetAlert("Error", "The cant be saved", "error");
+        window.sweetAlert("Error", "The cant be saved", "error");
       });
     }
   }

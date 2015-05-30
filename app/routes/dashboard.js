39bb1645197bb1b0bb4b8f73346cec3a6e44e342
Hector Zarco <hezarco@gmail.com>
2015-05-30 12:45:41 +0200

    Redirect user to campaign if there's not campaigns

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -4,6 +4,15 @@ import AuthenticatedRoute from "../mixins/authenticated-route";
 export default Ember.Route.extend(AuthenticatedRoute, {
   needsAuthentication: true,
   beforeModel: function() {
+    return this.get('store').find('campaign');
+  },
 
+  //Redirect to "/campaigns" if the user has no campaigns for force him to create one
+  redirect: function() {
+    var hasCampaigns = this.get('store').all('campaign').get('length');
+
+    if (!hasCampaigns) {
+      this.replaceWith('campaigns');
+    }
   }
 });
